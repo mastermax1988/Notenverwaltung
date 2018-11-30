d6011d7dbf3c91f234a36ca5011bfefe3196046d
max pc <huber.maxi@gmail.com>
2018-11-30 15:26:18 +0100

    demo working

--- server/server.py
+++ server/server.py
@@ -1,24 +1,35 @@
 
 import asyncio
 import datetime
-import random
 import websockets
 import json
 import shutil
 
 async def servefile(websocket, path):
-    f = open("noten.myjson","r")
-    await websocket.send(f.read())
     async for message in websocket:
-        if message:    
-            f = open("noten.myjson","w")
-            f.write(message)
+        if message == "load":
+            f = open("noten.myjson","r")
+            await websocket.send(f.read())
+            f.close()
+        elif message[0] == "{":
+            f = open("noten.myjson","r");
+            if message != f.read():
+                f.close()
+                backup()
+                f = open("noten.myjson","w")
+                f.write(message)
+                f.close()
+                print("new data saved");
+            else:
+                print("data unchanged, not saving");
+            f.close();
 
+def backup():
+    now = datetime.datetime.utcnow().isoformat() + 'Z'
+    path="backup/noten"+now+".myjson"
+    shutil.copy("noten.myjson",path)
+    print("Backup done!")
 
-now = datetime.datetime.utcnow().isoformat() + 'Z'
-path="backup/noten"+now+".myjson"
-shutil.copy("noten.myjson",path)
-print("Backup done!")
 
 start_server = websockets.serve(servefile, '127.0.0.1', 5678)
 
