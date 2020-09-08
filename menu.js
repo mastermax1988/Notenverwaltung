b7d1e86df60d699cb357f4465dca154d118dd44c
Max Huber <huber.maxi@gmail.com>
2020-09-08 20:38:43 +0200

    hinweis sitzplan

--- menu.js
+++ menu.js
@@ -1352,7 +1352,7 @@ function addPupilMenu()
 		return;
 	var className = sel[sel.selectedIndex].value;
 	var d = emptyForm1();
-	d.append("p").html("Am Ende unbedingt doppelte Leerzeichen in json löschen (sonst kein Springen zu mündlichen Noten möglich) :%s/__/_g");
+	d.append("p").html("Am Ende unbedingt doppelte Leerzeichen in json löschen (sonst kein Springen zu mündlichen Noten möglich) :%s/__/_g. Namen im Format Nachname, Vorname eintragen, da sonst kein Vorname für den Sitzplan erkannt wird.");
 	var pup = getPupils(className);
 	for(var i = 0; i < maxpupils; i++)
 	{
